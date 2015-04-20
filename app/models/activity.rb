947667e25e60c0946b087d2c38e8f5e5838768d8
aytigra <aytigra@gmail.com>
2015-04-20 18:45:26 +0700

    query activities by sectors + validations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,23 @@
 class Activity < ActiveRecord::Base
   belongs_to :subsector
+
+  validates :subsector, :name, presence: true
+
+  def self.activities_by_sectors(user)
+    raw = Activity.joins(:subsector).where(subsectors: {user_id: user.id}).order(:id, :subsector_id)
+            .select('activities.*, subsectors.name as subs_name, subsectors.sector_id as sector_id')
+
+    result = {}
+    Sector.keys.each { |k| result[k] = {} }
+
+    raw.each do |a|
+      if result[a.sector_id][a.subsector_id].present?
+        result[a.sector_id][a.subsector_id][:activities] << a
+      else
+        result[a.sector_id][a.subsector_id] = {name: a.subs_name, activities: [a]}
+      end
+    end
+
+    result
+  end
 end
--- app/models/subsector.rb
+++ app/models/subsector.rb
@@ -1,3 +1,7 @@
 class Subsector < ActiveRecord::Base
   belongs_to :user
+  has_many :activities
+
+  validates :user, :sector_id, :name, presence: true
+  validates :sector_id, inclusion: { in: Sector.keys, message: 'is wrong' }
 end
