f40e3639a75726e0902022eed5122034ce71257f
aytigra <aytigra@gmail.com>
2015-05-06 11:37:39 +0700

    Subsector destroy dependent activities

--- app/models/subsector.rb
+++ app/models/subsector.rb
@@ -1,6 +1,6 @@
 class Subsector < ActiveRecord::Base
   belongs_to :user
-  has_many :activities
+  has_many :activities, dependent: :destroy
 
   validates :user, :sector_id, :name, presence: true
   validates :sector_id, inclusion: { in: Sector.keys, message: 'is wrong' }
