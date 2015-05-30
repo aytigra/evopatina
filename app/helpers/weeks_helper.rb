f0e4da45d41b3304f33ec2ea7470dac043905186
aytigra <aytigra@gmail.com>
2015-05-30 11:34:44 +0700

    use locale for dates

--- app/helpers/weeks_helper.rb
+++ app/helpers/weeks_helper.rb
@@ -1,6 +1,6 @@
 module WeeksHelper
   def week_begin_end_text(week)
-    week.date.to_s(:date) + ' - ' + week.date.end_of_week.to_s(:date)
+    I18n.l(week.date, format: :short) + ' - ' + I18n.l(week.date.end_of_week, format: :short)
   end
 
   def week_ratio_text(week, sector_id)
