aeb8e3faac02d21e5353d7d2d49880fb8427448c
aytigra <aytigra@gmail.com>
2015-06-01 14:21:25 +0700

    prevent double js compression

--- webpack.config.js
+++ webpack.config.js
@@ -65,11 +65,4 @@ if (devBuild) {
   );
 } else {
   console.log('Webpack production build for Rails'); // eslint-disable-line no-console
-  config.plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    })
-  );
 }
