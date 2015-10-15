3980569b3c92f5d73a6b6d02eefa1da50aa3f478
aytigra <aytigra@gmail.com>
2015-10-15 19:35:19 +0300

    remove explicit progress recount on count change

--- app/controllers/fragments_controller.rb
+++ app/controllers/fragments_controller.rb
@@ -3,22 +3,15 @@ class FragmentsController < ApplicationController
 
   # PATCH/PUT /fragments/1.json
   def update
-    sector_id = params[:sector_id].to_i
     response_json = { id: @activity.id, subsector_id: @activity.subsector_id, sector_id: sector_id }
-    if sector_id > 0
-      fragment = Fragment.find_or_create(@activity, @week)
-      old_count = fragment.count || 0.0
-      fragment.count = params[:count].to_f
-      if fragment.save
-        @week.progress[sector_id] = @week.progress[sector_id] + fragment.count - old_count
-        @week.save
-        render json: response_json, status: :ok
-      else
-        response_json[:errors] = @activity.errors
-        render json: response_json, status: :unprocessable_entity
-      end
+    fragment = Fragment.find_or_create(@activity, @week)
+    fragment.count = params[:count].to_f
+    if fragment.save
+      render json: response_json, status: :ok
+    else
+      response_json[:errors] = @activity.errors
+      render json: response_json, status: :unprocessable_entity
     end
-
   end
 
   private
