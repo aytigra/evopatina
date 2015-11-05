d92a73aadef403f2b4edb8b0a513830d15288d92
aytigra <aytigra@gmail.com>
2015-11-05 13:42:51 +0300

    add sector.weeks init with sums

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -14,40 +14,34 @@ class Sector < ActiveRecord::Base
 
   def self.sectors_with_weeks(user, weeks)
     sectors = self.where(user: user)
-
     sector_weeks = SectorWeek.sector_weeks_by_sectors(sectors, weeks)
 
     sectors.each do |sector|
-      sector.weeks = sector_weeks[sector.id]
-      sector.weeks.each do |week|
-        week[1][:lapa_sum] = 0
-        week[1][:progress_sum] = 0
+      sector.weeks = Hash.new { |h,k| h[k] = {} }
+      #fill sector.weeks
+      #find sums of lapas and progresses by last 4 weeks for each week
+      #starting from last week
+      lapa_sum = 0.0
+      progress_sum = 0.0
+      (0..weeks.length-1).reverse_each do |i|
+        week_id = weeks[i].id
+
+        sector.weeks[week_id][:lapa] = sector_weeks[sector.id][week_id][:lapa] || 0.0
+        sector.weeks[week_id][:progress] = sector_weeks[sector.id][week_id][:progress] || 0.0
+
+        lapa_sum += sector.weeks[week_id][:lapa]
+        progress_sum += sector.weeks[week_id][:progress]
+
+        if i+4 < weeks.length #if not out of weeks range
+          week_out_id = weeks[i+4].id
+          lapa_sum -= sector.weeks[week_out_id][:lapa]
+          progress_sum -= sector.weeks[week_out_id][:progress]
+        end
+
+        sector.weeks[week_id][:lapa_sum] = lapa_sum
+        sector.weeks[week_id][:progress_sum] = progress_sum
       end
     end
-
-    # find sums of lapas and progresses by last 4 weeks
-    # starting from last week
-    # lapa_sum = {}
-    # progress_sum = {}
-    # (0..@weeks.length-1).reverse_each do |i|
-    #   @sectors.each do |sector|
-    #     lapa_sum[sector.id] ||= 0
-    #     progress_sum[sector.id] ||= 0
-    #     lapa_sum[sector.id] += @sector_weeks[sector.id][@weeks[i].id][:lapa]
-    #     progress_sum[sector.id] += @sector_weeks[sector.id][@weeks[i].id][:progress]
-    #     if i+4 < @weeks.length #if not out of range
-    #       lapa_sum[sector.id] -= @sector_weeks[sector.id][@weeks[i+4].id][:lapa]
-    #       progress_sum[sector.id] -= @sector_weeks[sector.id][@weeks[i+4].id][:progress]
-    #     end
-    #   end
-    #   @weeks[i].lapa_sum = lapa_sum.clone
-    #   @weeks[i].progress_sum = progress_sum.clone
-    #   #hmm
-    #   if @weeks[i].id == @current_week.id
-    #     @current_week = @weeks[i]
-    #   end
-    # end
-    #debug
 
     sectors
   end
