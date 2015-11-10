cb7ef6a5fe4a56a824af149e49e4bd69be601dd4
aytigra <aytigra@gmail.com>
2015-11-10 15:46:40 +0300

    Merge remote-tracking branch 'master' into custom_sectors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
-  has_many :weeks, dependent: :destroy
-  has_many :sectors, dependent: :destroy
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+
+  protected
+  def confirmation_required?
+    false
+  end
 end
