ee57c85bc3fbeee045db0637f74d6dc4f5212fa4
aytigra <aytigra@gmail.com>
2015-11-22 13:28:49 +0300

    add print button

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( bundle-weeks.js )
+Rails.application.config.assets.precompile += %w( bundle-weeks.js print.css)
 
 Rails.application.config.assets.configure do |env|
   #env.unregister_postprocessor 'application/javascript', Sprockets::SafetyColons
